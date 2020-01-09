708877b547ed689512834b66666cfab946c08249
KAUL, VINEET <kaul.vineet@gmail.com>
2020-01-09 16:56:58 +0530

    With Postgres

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,10 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+    import os
+    import psycopg2
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     return "Hello, World!"
