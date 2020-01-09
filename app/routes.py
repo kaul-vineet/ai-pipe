2dbf53f0e123d8e387cc8446ed00bd46a1f205fc
KAUL, VINEET <kaul.vineet@gmail.com>
2020-01-09 20:01:05 +0530

    With Postgres

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
 from app import app
+import psycopg2
+import sys, os
+import numpy as np
+import pandas as pd
 
 @app.route('/')
 @app.route('/index')
@@ -9,4 +13,19 @@ def index():
     DATABASE_URL = os.environ['DATABASE_URL']
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    print("Connected!")
+    
+    data = load_data('salesforce', 'case', conn)
+    
     return "Hello, World!"
+
+def load_data(schema, table, conn):
+
+    sql_command = "SELECT * FROM {}.{};".format(str(schema), str(table))
+    print (sql_command)
+
+    # Load the data
+    data = pd.read_sql(sql_command, conn)
+
+    print(data.shape)
+    return (data)
