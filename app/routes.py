cfe57fe8b1255cc33a03d8a6249da0aeb917395f
KAUL, VINEET <kaul.vineet@gmail.com>
2020-01-10 17:24:03 +0530

    With Postgres

--- app/routes.py
+++ app/routes.py
@@ -10,8 +10,8 @@ def index():
     import os
     import psycopg2
 
-    DATABASE_URL = os.environ['DATABASE_URL']
-
+    # DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = 'postgres://hmbadfwsbybxwc:6a9dd36753cacb0db21da38307159a534d85b41a1c511a40f0099cabc04f9680@ec2-107-21-97-5.compute-1.amazonaws.com:5432/dc7d4entntij7b'
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     print("Connected!")
     
