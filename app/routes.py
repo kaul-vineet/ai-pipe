500ad5795298da80b40b236dbe6e1ecdf844a61b
KAUL, VINEET <kaul.vineet@gmail.com>
2020-01-11 15:38:34 +0530

    With Postgres

--- app/routes.py
+++ app/routes.py
@@ -15,9 +15,9 @@ def index():
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     print("Connected!")
     
-    data = load_data('salesforce', 'case', conn)
+    score = load_data('salesforce', 'case', conn)
     
-    return "Hello, World!"
+    return score
 
 def load_data(schema, table, conn):
 
@@ -26,6 +26,26 @@ def load_data(schema, table, conn):
 
     # Load the data
     data = pd.read_sql(sql_command, conn)
+    
 
-    print(data.shape)
-    return (data)
+    data = data.drop(['external_id__c','subject','systemmodstamp','createddate','customer_type__c',
+                  'isdeleted','casenumber','sfid','id','_hc_lastop','_hc_err'], axis=1)
+    data['db_case_duration_hours__c'] = data['db_case_duration_hours__c'].fillna(data['db_case_duration_hours__c'].mode()[0])
+
+    cat_columns = ["priority", "origin"]
+    data = pd.get_dummies(data, prefix_sep="__", columns=cat_columns)
+
+    X_train, X_test, y_train, y_test = train_test_split(data.drop('isclosed',axis=1), data['isclosed'], test_size=0.30, random_state=101)
+
+    # instantiate the model (using the default parameters)
+    logreg = LogisticRegression()
+
+    # fit the model with data
+    logreg.fit(X_train,y_train)
+
+    #
+    y_pred=logreg.predict(X_test)
+
+    # Use score method to get accuracy of model
+    score = logreg.score(X_test, y_test)
+    return score
