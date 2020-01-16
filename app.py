373884e73fa91bb40f97a437e03b53f1dafba3b0
KAUL, VINEET <kaul.vineet@gmail.com>
2020-01-16 22:32:42 +0530

    new

--- app.py
+++ app.py
@@ -32,11 +32,12 @@ def create_plot(feature):
         N = 40
         x = np.linspace(0, 1, N)
         y = np.random.randn(N)
-        df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+        #df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+        df = load_data()
         data = [
             go.Bar(
-                x=df['x'], # assign x as the dataframe column 'x'
-                y=df['y']
+                x=df['model_timestamp'], # assign x as the dataframe column 'x'
+                y=df['model_score']
             )
         ]
     else:
@@ -62,46 +63,23 @@ def change_features():
     graphJSON= create_plot(feature)
     return graphJSON
 
-def load_data(schema, table, conn):
+def load_data():
     # DATABASE_URL = os.environ['DATABASE_URL']
     DATABASE_URL = 'postgres://hmbadfwsbybxwc:6a9dd36753cacb0db21da38307159a534d85b41a1c511a40f0099cabc04f9680@ec2-107-21-97-5.compute-1.amazonaws.com:5432/dc7d4entntij7b'
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     print("Connected!")
 
-    score = load_data('salesforce', 'case', conn)
+    c1 = "model_score"
+    c2 = "model_timestamp"
+    c3 = "model_recordcount"
 
-    sql_command = "SELECT * FROM {}.{};".format(str(schema), str(table))
+    sql_command = "SELECT * FROM {}.{}.{};".format(str(c1), str(c2), str(c3))
     print(sql_command)
 
     # Load the data
     data = pd.read_sql(sql_command, conn)
 
-    data = data.drop(['external_id__c','subject','systemmodstamp','createddate','customer_type__c',
-                  'isdeleted','casenumber','sfid','id','_hc_lastop','_hc_err'], axis=1)
-    data['db_case_duration_hours__c'] = data['db_case_duration_hours__c'].fillna(data['db_case_duration_hours__c'].mode()[0])
-
-    cat_columns = ["priority", "origin"]
-    data = pd.get_dummies(data, prefix_sep="__", columns=cat_columns)
-
-    X_train, X_test, y_train, y_test = train_test_split(data.drop('isclosed',axis=1), data['isclosed'], test_size=0.30, random_state=101)
-
-    # instantiate the model (using the default parameters)
-    logreg = LogisticRegression()
-
-    # fit the model with data
-    logreg.fit(X_train,y_train)
-
-    #
-    y_pred=logreg.predict(X_test)
-
-    # Use score method to get accuracy of model
-    score = logreg.score(X_test, y_test)
-
-    cur = conn.cursor()
-    cur.execute("INSERT INTO public.modelmetrics(model_score, model_timestamp) VALUES (%s, %s)", (truncate(score, 3), str(datetime.datetime.now())))
-    conn.commit()
-
-    return str(score)
+    return data
 
 
 def truncate(n, decimals=0):
